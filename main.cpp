713d8aa3a15f5f9f50ea48d02b6aed63d58c9100
Etten32 <etten327vcr@gmail.com>
2024-02-16 08:45:15 -0500

    Initial commit: added main with rough program outline

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//A Bubble Below: a basic submarine simulator program to refresh C++
+#include <stdio.h>
+int main(int argc, char* args[]){
+    //SETUP PROGRAM
+    //variables
+    //helper functions
+
+    //RUN PROGRAM
+    //display screen
+    //intake prompt
+    //run action
+
+    //SHUTDOWN PROGRAM
+    //release variables
+
+    printf("END");
+    return 1;
+}
